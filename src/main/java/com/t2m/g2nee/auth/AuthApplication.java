4d85d32a00e4cc66e328286fac02e73aa094813a
origin <git@github.com:ehdals0405/PreProjectback.git>
2024-03-14 14:06:05 +0900

    :sparkles: CI Setting

---
+++ src/main/java/com/t2m/g2nee/auth/AuthApplication.java
@@ -0,0 +1,13 @@
+package com.t2m.g2nee.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+
+}
