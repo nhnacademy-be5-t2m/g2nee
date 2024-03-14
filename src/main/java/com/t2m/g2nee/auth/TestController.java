1c41a1b40896d081f9e4468850a6123f95dc2d1b
origin <git@github.com:ehdals0405/PreProjectback.git>
2024-03-14 16:16:36 +0900

    :sparkles: CD Setting

--- src/main/java/com/t2m/g2nee/auth/TestController.java
+++ src/main/java/com/t2m/g2nee/auth/TestController.java
@@ -1,14 +1,14 @@
 package com.t2m.g2nee.auth;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class testController {
+@RestController
+public class TestController {
 
     @GetMapping("/test")
     public String hello(){
 
-        return "Hello Test!";
+        return "hello";
     }
 }
