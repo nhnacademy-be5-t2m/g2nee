61722f26987c165ae5cf0a4552400c1ed60eb8c9
정지은 <68415975+ssstopeun@users.noreply.github.com>
2024-04-18 10:00:04 +0900

    Revert "Auth Server: 로그인, 로그아웃과 관련된 토큰 발급, 삭제처리"

--- src/main/java/com/t2m/g2nee/auth/exception/token/MemberDTOParsingException.java
+++
@@ -1,9 +0,0 @@
-package com.t2m.g2nee.auth.exception.token;
-
-public class MemberDTOParsingException extends RuntimeException {
-    private static final String MESSAGE = "유저 정보를 파싱할 수 없습니다";
-
-    public MemberDTOParsingException() {
-        super(MESSAGE);
-    }
-}
